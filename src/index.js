ff367b151ae7c5da53fa84cdddcd7e438146c887
jhonecode <joao.pedro100@hotmail.com.br>
2022-08-03 18:35:45 -0300

    db connection timeout fix

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ const app = express();
 mongoose.connect(
   process.env.MONGO_URL,
   {
+    reconnectTries: 30,
+    reconnectInterval: 500,
+    poolSize: 1,
+    socketTimeoutMS: 2000000,
+    keepAlive: true,
     useNewUrlParser: true
   }
 );
